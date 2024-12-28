7ab44136d85b4fe9c893e00ecb5fb03aacefb8dd
Jaks <jcqs.gr@gmail.com>
2024-12-28 20:59:53 +0100

    delete old folder

--- old/src/domain/noise/noise.ts
+++
@@ -1,9 +0,0 @@
-import Position from "../shared/position";
-
-export default class Noise {
-    position: Position;
-
-    constructor(position: Position) {
-        this.position = position
-    }
-}
