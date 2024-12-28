731280aa8bc7d4a87d70f0fb722eabbe93261b3c
Jaks <jcqs.gr@gmail.com>
2024-12-28 12:59:18 +0100

    refacto

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
 import Game from "./src/domain/game";
 import Level from "./src/domain/level";
 import WebBrowserGame from "./src/infrastructure/web_browser_game";
-import LevelConfig from "./src/domain/interface/level_config";
 
-let level1Config: LevelConfig = {
+let level1 = new Level({
     size: 6,
     boxes: [
         {
@@ -11,19 +10,12 @@ let level1Config: LevelConfig = {
             z:2
         }
     ]
-}
-
-
-let level1 = new Level(level1Config)
-
-
-let levels = [
-    level1
-];
-let gameConfig = {
-    levels: levels
-}
-let game = new Game(gameConfig);
+})
+let game = new Game({
+    levels: [
+        level1
+    ]
+});
 
 let browserGame = WebBrowserGame.fromGame(game);
 let container = document.createElement('div');
