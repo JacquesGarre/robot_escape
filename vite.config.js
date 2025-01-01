33f5dcc6a82adda23fb83352dde441a17a46620b
Jaks <jcqs.gr@gmail.com>
2025-01-01 21:40:48 +0100

    assets

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/robot_escape/',
-  resolve: {
-    alias: {
-      '/assets/': '/robot_escape/assets/',
-    },
-  },
+  base: '/robot_escape/'
 });
