b5ca0905d6967ce927ebdb0b8d10a6dbbe44a6d1
Jacques Garré <jcqs.gr@gmail.com>
2025-01-21 21:11:07 +0100

    Update physical_coordinates.ts

--- src/domain/physical_coordinates.ts
+++ src/domain/physical_coordinates.ts
@@ -1,6 +1,6 @@
-export default class PhysicalCoordinates {
+export default class PhysicalCoordinates {  // TODO: rename to 3dCoordinates? -> it should be composed of 2dCoordinates / GridCoordinates?! those are obviously linked
     
-    x: number;
+    x: number;  // TODO: Value objects.
     y: number;
     z: number;
 
