129820b103571c60d2cb901fcb1235aa8cd6201c
Jacques Garré <jcqs.gr@gmail.com>
2025-01-21 20:59:37 +0100

    Update game.ts

--- src/domain/game.ts
+++ src/domain/game.ts
@@ -6,20 +6,20 @@ import Level from "./level";
 
 export default class Game {
 
-    levels: Level[]
-    currentLevelIndex: number;
+    levels: Level[] // TODO : Levels value objects?
+    currentLevelIndex: number; // TODO : could be in the Levels value objects ?
     controls: Controls;
-    gameOver: boolean = false;
-    levelsConfig: LevelConfig[]
+    gameOver: boolean = false; // TODO : have a state instead
+    levelsConfig: LevelConfig[]  // TODO : could be in the Levels value objects ?
 
     constructor(config: GameConfig) {
         this.currentLevelIndex = 0;
         this.levelsConfig = config.levels
-        this.levels = this.initializeLevels();
+        this.levels = this.initializeLevels(); // TODO : Levels.fromConfig() 
         this.controls = new Controls();
     }
 
-    initializeLevels(): Level[] {
+    initializeLevels(): Level[] { // TODO : Move to Levels.fromConfig() 
         let levels: Level[] = [];
         for(const levelConfig of this.levelsConfig) {
             levels.push(new Level(levelConfig));
@@ -27,46 +27,46 @@ export default class Game {
         return levels;
     }
 
-    previousLevel(): Level | undefined {
+    previousLevel(): Level | undefined { // TODO : could be in the Levels value objects  (game.levels.previous())
         if ((this.currentLevelIndex-1) < 0 || (this.currentLevelIndex-1) >= this.levels.length) {
             return;
         }
         return this.levels[this.currentLevelIndex-1];
     }
 
-    currentLevel(): Level {
+    currentLevel(): Level { // TODO : could be in the Levels value objects (game.levels.current())
         if (this.currentLevelIndex < 0 || this.currentLevelIndex >= this.levels.length) {
             throw new LevelNotFoundError(`Level index ${this.currentLevelIndex} does not exist`);
         }
         return this.levels[this.currentLevelIndex];
     }
 
-    nextLevel(): Level | undefined {
+    nextLevel(): Level | undefined { // TODO : (game.levels.next())
         if ((this.currentLevelIndex+1) < 0 || (this.currentLevelIndex+1) >= this.levels.length) {
             return;
         }
         return this.levels[this.currentLevelIndex+1];
     }
 
-    startNextLevel() {
+    startNextLevel() { // TODO :  game.levels.next().start() ?
         let nextLevelIndex = this.currentLevelIndex + 1;
         this.levels[nextLevelIndex].synchronize(this.currentLevel());
         this.currentLevelIndex = nextLevelIndex;
     }
 
-    restartLevel() {
+    restartLevel() { // TODO :  game.levels.current().restart() ?
         this.gameOver = false;
         const currentLevelConfig = this.levelsConfig.find(obj => obj.index === this.currentLevelIndex) as LevelConfig;
         this.levels[this.currentLevelIndex] = new Level(currentLevelConfig);
     }
 
-    animate() {
+    animate() { 
         let currentLevel = this.currentLevel();
         if (currentLevel.gameOver) {
             this.gameOver = true;
         }
         currentLevel.animate(this.controls);
-        if (currentLevel.finished) {
+        if (currentLevel.finished) { 
             this.startNextLevel();
         }
     }
