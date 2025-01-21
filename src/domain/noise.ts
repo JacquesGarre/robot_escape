20c68c7f66d09ecf2e7c9127fb11873c98453860
Jacques Garré <jcqs.gr@gmail.com>
2025-01-21 21:09:37 +0100

    Update noise.ts

--- src/domain/noise.ts
+++ src/domain/noise.ts
@@ -2,7 +2,7 @@ import LevelObject from "./level_object";
 import LevelObjectType from "./level_object_type";
 import Robot from "./robot";
 
-export default class Noise extends LevelObject {
+export default class Noise extends LevelObject { // TODO: Shouldn't extend level object...
 
     constructor(x: number, z: number) {
         super({
