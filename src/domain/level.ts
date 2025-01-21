5963c900ce80f41ab7f30684924a97e534cb0c86
Jacques Garré <jcqs.gr@gmail.com>
2025-01-21 21:03:55 +0100

    Update level.ts

--- src/domain/level.ts
+++ src/domain/level.ts
@@ -11,18 +11,18 @@ export default class Level {
 
     static TILESIZE = 5;
 
-    index: number;
-    name: string;
-    description: string;
-    size: number;
-    camera: Camera;
-    boxes: Box[] = [];
+    index: number; // TODO : Value object
+    name: string;  // TODO : Value object
+    description: string; // TODO : Value object
+    size: number; // TODO : Value object
+    camera: Camera; // TODO : Move to infra
+    boxes: Box[] = [];  // TODO : Value object boxes
     robot: Robot;
     elevator: Elevator;
     boundaries: Boundaries;
-    finished: boolean;
-    enemies: Enemy[] = [];
-    gameOver: boolean = false;
+    finished: boolean; // TODO : LevelState
+    enemies: Enemy[] = []; // TODO : Enemies value object
+    gameOver: boolean = false; // TODO : LevelState
 
     constructor(config: LevelConfig) {  
         this.index = config.index;      
@@ -30,14 +30,14 @@ export default class Level {
         this.description = config.description;
         this.size = config.size;
         let boxesConfig = config.boxes ?? [];
-        for(const boxConfig of boxesConfig) {
+        for(const boxConfig of boxesConfig) { // TODO : Boxes.fromConfig(...)
             let box = new Box(boxConfig);
-            this.boxes.push(box);
+            this.boxes.push(box); 
         }
         this.robot = new Robot(config.robot);
         this.camera = Camera.fromRobot(this.robot);
         this.elevator = new Elevator(config.elevator);
-        this.boundaries = new Boundaries({
+        this.boundaries = new Boundaries({    // TODO : Boundaries.fromConfig()
             xMin: 0, 
             xMax: this.size * Level.TILESIZE,
             yMin: 0,
@@ -46,31 +46,31 @@ export default class Level {
             zMax: this.size * Level.TILESIZE, 
         })
         let enemiesConfig = config.enemies ?? [];
-        for(const enemyConfig of enemiesConfig) {
+        for(const enemyConfig of enemiesConfig) { // TODO : Enemies.fromConfig()
             let enemy = new Enemy(enemyConfig);
             this.enemies.push(enemy)
         }
     }
 
-    animate(controls: Controls) {
+    animate(controls: Controls) { // TODO : State pattern
         if (this.robot.isDead) {
             this.gameOver = true;
         }
-        this.robot.animate(controls, this);
-        this.camera.follow(this.robot);
-        if (this.robot.isOnElevator(this.elevator)) {
+        this.robot.animate(controls, this); 
+        this.camera.follow(this.robot);  // TODO : Infra layer
+        if (this.robot.isOnElevator(this.elevator)) {   // TODO : State pattern
             this.robot.setBoundaries(this.elevator.edges())
-            this.elevator.goUp(this.robot);
+            this.elevator.goUp(this.robot); 
             if (this.elevator.hasReachedTop()) {
                 this.finished = true;
             }
         }
-        for(const enemy of this.enemies) {
+        for(const enemy of this.enemies) { // TODO : enemies.animate()
             enemy.animate(this)
         }
     }
 
-    gridRepresentation() {
+    gridRepresentation() { // TODO : Go for some utils 
         const levelGrid = Array.from({ length: this.size }, () =>
             Array(this.size).fill(0)
         );
@@ -82,7 +82,7 @@ export default class Level {
         return levelGrid
     }
 
-    synchronize(level: Level) {
+    synchronize(level: Level) { 
         this.robot.center.x = level.robot.center.x;
         this.robot.center.z = level.robot.center.z;
         this.robot.rotation = level.robot.rotation;
